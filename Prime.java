e8c8efc4669bdab4b963dea304fd9bed56084662
Shubham Singh Bhadouria <96174262+Shubhambhadouria@users.noreply.github.com>
2022-05-26 21:01:05 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,9 +1,11 @@
 package com.massai;
 
 public class Prime{
+	
 public int[] findAndReturnPrimeNumbers(int[] inputArray){
 
-			
+	int[] ans = new int[7];
+	int s = 0;
 	for(int i=0; i<inputArray.length; i++)
 	{
 		int count=0;
@@ -13,24 +15,37 @@ public int[] findAndReturnPrimeNumbers(int[] inputArray){
 			{
 				count++;
 			}
-			if(count>2)
-			{
-				return 0;
-			}
-			else
-			{
-			
-			}
+		}
+		if(count==2)
+		{
+			ans[s]=inputArray[i];
+			s++;
 		}
 	}
+	return ans;
 }
 public static void main(String[] args){
 
- int[] arr = {10,12,5,50,11,14,15};
+	Prime p1 = new Prime();
+	int[] arr = {10,12,5,50,11,14,15};
+	int[] result = p1.findAndReturnPrimeNumbers(arr);
+  
+  
+	for(int i=0; i<result.length; i++) {
+		if(result[0]==0) {
+			System.out.println("Prime number not found in the supplied Array");
+			break;
+		}
+		else if(result[i]!=0) {
+			System.out.println(result[i]);
+		}
+		else {
+			break;
+		}
+		
+	  }
+  
 
- 
-  Prime p1 = new Prime();
-  p1.findAndReturnPrimeNumbers(arr);
  
 }
 }
